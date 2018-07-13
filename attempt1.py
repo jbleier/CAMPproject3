56a5a04d201d11f47bfc325c1fe273f045dd5be5
Pranav Garg <pranavgarg@gmail.com>
2018-07-13 16:20:08 +0530

    added SVM

--- attempt1.py
+++ attempt1.py
@@ -6,10 +6,43 @@ import numpy as np
 from matplotlib import pyplot as plt
 pi = np.pi
 from brian2 import *
+from sklearn.svm import SVC # "Support vector classifier"
+model = SVC(kernel='linear', C=1E10)
 
-#Create some data
+# Create some data
 
 from coding import single_response_frequency
+
+
+
+
+
+def plot_svc_decision_function(model, ax=None, plot_support=True):
+    """Plot the decision function for a 2D SVC"""
+    if ax is None:
+        ax = plt.gca()
+    xlim = ax.get_xlim()
+    ylim = ax.get_ylim()
+    
+    # create grid to evaluate model
+    x = np.linspace(xlim[0], xlim[1], 30)
+    y = np.linspace(ylim[0], ylim[1], 30)
+    Y, X = np.meshgrid(y, x)
+    xy = np.vstack([X.ravel(), Y.ravel()]).T
+    P = model.decision_function(xy).reshape(X.shape)
+    
+    # plot decision boundary and margins
+    ax.contour(X, Y, P, colors='k',
+               levels=[-1, 0, 1], alpha=0.5,
+               linestyles=['--', '-', '--'])
+    
+    # plot support vectors
+    if plot_support:
+        ax.scatter(model.support_vectors_[:, 0],
+                   model.support_vectors_[:, 1],
+                   s=300, linewidth=1, facecolors='none');
+    ax.set_xlim(xlim)
+
 N=20 # number of neurons
 n_stim = 100 # number of stimuli
 stimuli = np.linspace(0, 2*pi, n_stim) # create 100 stimuli from 0-2pi
@@ -29,23 +62,28 @@ preferred_orientations = stimuli[np.argmax(avg_responses, axis=1)] % pi
 
 # Without correlations
 st1 = 2.  # orientation of the two stimuli
-st2 = st1 + 1.
+st2 = st1 +1 
+
 n0, n1 = 0, 1  # choose two neurons
 npoints = 100
-
 stims1 = st1*np.ones(npoints)
-resp0 = single_response_frequency(stims1, n0)
-resp1 = single_response_frequency(stims1, n1)
-plt.subplot(121)
-plt.scatter(resp0, resp1, alpha=0.4,
+resp0_1 = single_response_frequency(stims1, n0)
+resp1_1 = single_response_frequency(stims1, n1)
+plt.figure(1)
+plt.scatter(resp0_1, resp1_1, alpha=0.4,
             label="Stimulus orientation {} rad".format(st1))
-
+X_1 = np.column_stack((resp0_1, resp1_1))
+Y_1 = np.zeros((len(resp0_1)))
 stims2 = st2*np.ones(npoints)
-resp0 = single_response_frequency(stims2, n0)
-resp1 = single_response_frequency(stims2, n1)
-plt.scatter(resp0, resp1, alpha=0.4,
+resp0_2 = single_response_frequency(stims2, n0)
+resp1_2 = single_response_frequency(stims2, n1)
+plt.scatter(resp0_2, resp1_2, alpha=0.4,
             label="Stimulus orientation {} rad".format(st2))
-
+X_2 = np.column_stack((resp0_2, resp1_2))
+Y_2 = np.ones((len(resp0_1)))
+X = np.concatenate((X_1, X_2))
+Y = np.concatenate((Y_1, Y_2))
+#print(X)
 plt.ylim([0, 100])
 plt.xlim([0, 100])
 plt.xlabel("Response of neuron {}".format(n0))
@@ -54,29 +92,32 @@ plt.legend()
 
 plt.gca().set_aspect("equal")
 
-
+model.fit(X, Y)
+plot_svc_decision_function(model);
 # Now add trivial noise correlations
 
 noise_intensity = 10
-angle = pi/4
+angle = pi/8
+for i in range(17):
+	noise = np.random.normal(0, noise_intensity, size=(2, npoints))
+	plt.figure(2)
+	resp0toS1 = single_response_frequency(stims1, n0) + noise[0]*np.sin(i * angle)
+	resp1toS1 = single_response_frequency(stims1, n1) + noise[0]*np.cos(i * angle)
+	plt.scatter(resp0toS1, resp1toS1, alpha=0.4,
+	            label="Stimulus orientation {} rad".format(i*angle))
 
-noise = np.random.normal(0, noise_intensity, size=(2, npoints))
-plt.subplot(122)
-resp0toS1 = single_response_frequency(stims1, n0) + noise[0]*np.sin(angle)
-resp1toS1 = single_response_frequency(stims1, n1) + noise[0]*np.cos(angle)
-plt.scatter(resp0toS1, resp1toS1, alpha=0.4,
-            label="Stimulus orientation {} rad".format(st1))
+	resp0toS2 = single_response_frequency(stims2, n0) + noise[1]*np.sin(i * angle)
+	resp1toS2 = single_response_frequency(stims2, n1) + noise[1]*np.cos(i * angle)
+	plt.scatter(resp0toS2, resp1toS2, alpha=0.4,
+	            label="Stimulus orientation {} rad".format(i*angle))
 
-resp0toS2 = single_response_frequency(stims2, n0) + noise[1]*np.sin(angle)
-resp1toS2 = single_response_frequency(stims2, n1) + noise[1]*np.cos(angle)
-plt.scatter(resp0toS2, resp1toS2, alpha=0.4,
-            label="Stimulus orientation {} rad".format(st2))
+	plt.ylim([0, 120])
+	plt.xlim([0, 120])
+	plt.xlabel("Response of neuron {}".format(n0))
+	plt.ylabel("Response of neuron {}".format(n1))
+	plt.legend()
+	plt.gca().set_aspect("equal")
+	plt.draw()
+	plt.pause(1)
+	plt.clf()
 
-plt.ylim([0, 120])
-plt.xlim([0, 120])
-plt.xlabel("Response of neuron {}".format(n0))
-plt.ylabel("Response of neuron {}".format(n1))
-plt.legend();
-
-plt.gca().set_aspect("equal")
-show()
